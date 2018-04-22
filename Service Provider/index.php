0a00fc01fce026af45e385b7b9bb3a22dcb31c86
jellyaace <2152940@slu.edu.ph>
2018-04-23 02:33:36 +0800

    Made the structure of index

--- Service Provider/index.php
+++ Service Provider/index.php
@@ -4,14 +4,69 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
-<div class="slider">
-		<div class="load">
+<body>
+	<div class ="parallax">
+		<div class="slider">
+				<div class="load">
+				</div>
+				<div class="content">
+					<div class="principal">
+						<p>Serve the world with sophistication</p>
+						<a href="login.php">Go to Madamemoiselle</a>
+					</div>
+				</div>
 		</div>
-		<div class="content">
-			<div class="principal">
-			<a href="login.php">Sign In</a>
+	</div>
+	
+	<div class ="main-index">
+		<div class ="main-index1">
+			<div class ="main-index1-1">
+			</div>
+			<div class ="main-index1-2">
+				<p><strong>Share</strong> your creations!</p>
 			</div>
 		</div>
-</div>
+		
+		<div class ="main-index2">
+			<div class ="main-index2-1">
+				<p>Accept/Reject</p>
+			</div>
+			<div class ="main-index2-2">	
+			</div>
+		</div>
+		
+		<div class ="main-index3">
+			<div class ="main-index3-1">
+			</div>
+			<div class ="main-index3-2">
+				<p>Planner</p>
+			</div>
+		</div>
+		
+		<div class ="main-index4">
+			<div class ="main-index4-1">
+				<p>How to upload 1</p>
+			</div>
+			<div class ="main-index4-2">
+				<p>How to upload 2</p>
+			</div>
+			<div class ="main-index4-3">	
+				<p>How to upload 3</p>
+			</div>
+		</div>
+		
+		<div class ="main-index5">
+			<div class="main-index5-container">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. 
+				Curabitur tortor. Pellentesque nibh. Aenean quam.</p>
+			</div>
+		</div>		
+	</div>
+
+	<footer>
+		<P>© 2018 M A D A M E O I S E L L E . C O M </P><BR>
+		<P>All Rights Reserved</P>
+	</footer>
+</body>
 
 </html>
