8fb7a24263db6babe91640e2c29d25f9c41333ca
jellyaace <2152940@slu.edu.ph>
2018-04-23 00:16:31 +0800

    Coded the login and logout

---
+++ Service Provider/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Rental</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<div class="slider">
+		<div class="load">
+		</div>
+		<div class="content">
+			<div class="principal">
+			<a href="login.php">Sign In</a>
+			</div>
+		</div>
+</div>
+
+</html>
